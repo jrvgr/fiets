ad596edbd60b60e28bea54817b4bd7b72b448944
Jacco Groen <jacco@groen.fyi>
2024-10-10 16:18:24 +0200

    feat: rework the history card ui

--- src/components/history/history.svelte
+++ src/components/history/history.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import dayjs from "dayjs";
+  import calendar from "dayjs/plugin/calendar";
+  dayjs.extend(calendar);
+
   export let date: Date;
   export let row: string;
   export let location: string;
   export let spot: string;
 </script>
 
-<div class="flex flex-col gap-2">
-  <div class="flex flex-col gap-1">
+<div class="grid gap-4 grid-rows-2 grid-cols-3">
+  <div class="flex flex-col gap-1 col-span-3">
     <span class="font-bold">Date:</span>
-    <span>{date.toString()}</span>
+    <span>{dayjs(date).calendar()}</span>
   </div>
   <div class="flex flex-col gap-1">
     <span class="font-bold">Location:</span>
