935218a95194ddf5d570b2c65603e85169279ce3
Jacco Groen <jacco@groen.fyi>
2024-10-10 15:33:15 +0200

    chore: update modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import postcss from './postcss.config.cjs'
+import { fileURLToPath, URL } from 'node:url'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   css: {
     postcss
+  },
+  resolve: {
+    alias: [
+      { find: "@", replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+    ]
   }
 })
