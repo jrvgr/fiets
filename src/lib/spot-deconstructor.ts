1df3d800cbbe6ae46952f4b4e5651e5958b6973d
Jacco Groen <jacco@groen.fyi>
2024-10-10 15:35:56 +0200

    feat: spot deconstructor impl

---
+++ src/lib/spot-deconstructor.ts
@@ -0,0 +1,24 @@
+export type Item = {
+  readonly location: string;
+  readonly row: string;
+  readonly spot: string;
+  readonly date: Date;
+};
+
+// [location (2ch)][row (2ch)][spot (rest of numbers)]
+
+export function spotDeconstructor(originalString: string, url = true): Item {
+  const spot = url
+    ? (originalString.split("/").pop() as string)
+    : originalString;
+
+  console.log(spot);
+  const location = spot.slice(0, 2);
+  const row = spot.slice(2, 4);
+  const spotNum = spot.slice(4);
+  return { location, row, spot: spotNum, date: new Date() };
+}
+
+export function spotConstructor({ row, location, spot, date }: Item): string {
+  return `${location}${row}${spot}@${new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(date)}`;
+}
