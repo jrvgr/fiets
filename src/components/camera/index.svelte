a465ecdb64ca08b7dd603af0ed0fc02a320f92ee
Jacco Groen <jacco@groen.fyi>
2024-10-10 15:55:48 +0200

    bugfix: change text to say history instead of settings

--- src/components/camera/index.svelte
+++ src/components/camera/index.svelte
@@ -1,18 +1,16 @@
 <script lang="ts">
-    import { settings, type Setting } from "@/lib/settings";
-    import Agnostic from "./agnostic.svelte";
-    import BarcodeDetector from "./barcode-detector.svelte";
-    import type { Writable } from "svelte/store";
+  import { settings, type Setting } from "@/lib/settings";
+  import Agnostic from "./agnostic.svelte";
+  import BarcodeDetector from "./barcode-detector.svelte";
+  import type { Writable } from "svelte/store";
 
-    const barcodeDetector = settings.get(
-        "barcodeDetector",
-    ) as Writable<Setting>;
+  const barcodeDetector = settings.get("barcodeDetector") as Writable<Setting>;
 </script>
 
 {#key $barcodeDetector}
-    {#if $barcodeDetector.value}
-        <BarcodeDetector on:scanned />
-    {:else}
-        <Agnostic on:scanned />
-    {/if}
+  {#if $barcodeDetector.value}
+    <BarcodeDetector on:scanned />
+  {:else}
+    <Agnostic on:scanned />
+  {/if}
 {/key}
