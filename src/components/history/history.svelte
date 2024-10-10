49db8b75f4518b16bd9bb735831bcf5bb221d943
Jacco Groen <jacco@groen.fyi>
2024-10-10 15:56:01 +0200

    chore: formatting

--- src/components/history/history.svelte
+++ src/components/history/history.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-    export let date: Date;
-    export let row: string;
-    export let location: string;
-    export let spot: string;
+  export let date: Date;
+  export let row: string;
+  export let location: string;
+  export let spot: string;
 </script>
 
 <div class="flex flex-col gap-2">
-    <div class="flex flex-col gap-1">
-        <span class="font-bold">Date:</span>
-        <span>{date.toString()}</span>
-    </div>
-    <div class="flex flex-col gap-1">
-        <span class="font-bold">Location:</span>
-        <span>{location}</span>
-    </div>
-    <div class="flex flex-col gap-1">
-        <span class="font-bold">Spot:</span>
-        <span>{spot}</span>
-    </div>
-    <div class="flex flex-col gap-1">
-        <span class="font-bold">Rows:</span>
-        <span>{row}</span>
-    </div>
+  <div class="flex flex-col gap-1">
+    <span class="font-bold">Date:</span>
+    <span>{date.toString()}</span>
+  </div>
+  <div class="flex flex-col gap-1">
+    <span class="font-bold">Location:</span>
+    <span>{location}</span>
+  </div>
+  <div class="flex flex-col gap-1">
+    <span class="font-bold">Spot:</span>
+    <span>{spot}</span>
+  </div>
+  <div class="flex flex-col gap-1">
+    <span class="font-bold">Rows:</span>
+    <span>{row}</span>
+  </div>
 </div>
